8058d88443a7b9f0b0601efc0815d79b6c3bca05
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-01-17 18:06:23 +1100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,267 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TrickyMaddnessLevelHook
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Harmony harmony;
+        public static MenuManager menuManager;
+
+        public static AssetBundle myLoadedAssetBundle;
+        public static string LoadedAssetBundle = "";
+        public static string LoadedTrack;
+        public static string LoadedTrackName;
+
+        public static List<string> TrackNames = new List<string>();
+        public static List<string> Paths = new List<string>();
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + "\\Maps\\"))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\Maps\\");
+            }
+            AddTracks();
+            DoPatching();
+        }
+        public void AddTracks()
+        {
+            var List = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Maps\\", "*.asset");
+            for (int i = 0; i < List.Length; i++)
+            {
+                Paths.Add(List[i]);
+                TrackNames.Add(Path.GetFileName(List[i]).Split(".")[0]);
+            }
+        }
+
+        public static void DoPatching()
+        {
+            harmony = new Harmony("com.glitcherog.patch");
+            //Level appears
+            var mOriginal = AccessTools.Method(typeof(MenuManager), "RegisterLevels"); // if possible use nameof() here
+            var mPrefix = SymbolExtensions.GetMethodInfo(() => RegisterLevels());
+
+            harmony.Patch(mOriginal, null, new HarmonyMethod(mPrefix));
+
+            mOriginal = AccessTools.Method(typeof(MakerLevelData), "Awake"); // if possible use nameof() here
+            mPrefix = SymbolExtensions.GetMethodInfo(() => LoadLevelManager());
+            harmony.Patch(mOriginal, null, new HarmonyMethod(mPrefix));
+
+            harmony.PatchAll();
+        }
+
+        public static void RegisterLevels()
+        {
+            menuManager = MenuManager.Instance;
+
+            for (int i = 0; i < Paths.Count; i++)
+            {
+                LevelEntries.RegisterLevel(new LevelEntry
+                {
+                    name = TrackNames[i],
+                    sceneName = "$" + Paths[i],
+                    scoreThresholds = new float[]
+                    {
+                    100000f,
+                    150000f,
+                    200000f
+                    },
+                    timeThresholds = new float[]
+                    {
+                    110f,
+                    100f,
+                    95f
+                    },
+                    freestyleTimeLimit = 150f
+                });
+            }
+        }
+
+        public static void LoadLevelManager()
+        {
+            //Find Main Starting Objects
+            var levelManagerMaker = FindAnyObjectByType<MakerLevelData>();
+            var MakerSpawnPoint = FindAnyObjectByType<MakerSpawnPoint>();
+            var MakerRespawnPoint = FindAnyObjectByType<MakerRespawnPoints>();
+            //Find AI Paths Object
+
+            //Update Level Entry
+            var LevelEntry = menuManager.lastLevelEntry;
+
+            LevelEntry.timeThresholds[2] = levelManagerMaker.GoldTime;
+            LevelEntry.timeThresholds[1] = levelManagerMaker.SilverTime;
+            LevelEntry.timeThresholds[0] = levelManagerMaker.BronzeTime;
+
+            LevelEntry.scoreThresholds[2] = levelManagerMaker.GoldPoints;
+            LevelEntry.scoreThresholds[1] = levelManagerMaker.SilverPoints;
+            LevelEntry.scoreThresholds[0] = levelManagerMaker.BronzePoints;
+
+            LevelEntry.freestyleTimeLimit = levelManagerMaker.TimeLeft;
+
+            Traverse.Create(menuManager).Field("lastLevelEntry").SetValue(LevelEntry);
+
+            //Insert Starting Gate
+            var StartingGate = Resources.Load<GameObject>("game/startinggate");
+            GameObject StartingGateObject = Instantiate(StartingGate);
+            StartingGateObject.transform.position = MakerSpawnPoint.transform.TransformPoint(new Vector3(-0.5f,0,0.5f));
+            StartingGateObject.transform.rotation = MakerSpawnPoint.transform.rotation;
+
+            //Insert Level Manager
+            var LevelManagerObject = Resources.Load<GameObject>("game/levelmanager");
+            var gameObject = Instantiate(LevelManagerObject, levelManagerMaker.transform);
+
+            var levelManager = gameObject.GetComponent<LevelManager>();
+
+            //Set Level Manager Data Points
+            Traverse.Create(levelManager).Field("startingGate").SetValue(MakerSpawnPoint.transform);
+            Traverse.Create(levelManager).Field("respawnPointsParent").SetValue(MakerRespawnPoint.transform);
+            Traverse.Create(levelManager).Field("snowflakes").SetValue(new GameObject("Temp")); //FIX to get all snowflakes and apply
+
+            //Create UI Hooks
+            var TempObject = Traverse.Create(levelManager).Field("speedLabel").GetValue() as TextMeshProUGUI;
+            var CanvasObjects = FindObjectsByType(typeof(MakerCanvas), FindObjectsSortMode.None) as MakerCanvas[];
+            for (int i = 0; i < CanvasObjects.Length; i++)
+            {
+                GameObject gameObject1 = Instantiate(TempObject.transform.gameObject, levelManager.transform.GetChild(0));
+                gameObject1.SetActive(true);
+                gameObject1.GetComponent<TextMeshProUGUI>().text = "NewText";
+                CanvasObjects[i].Generate(gameObject1.GetComponent<TextMeshProUGUI>());
+            }
+
+            //Convert From Make Rails to Game Rails
+            var RailObjects = FindObjectsByType(typeof(MakerRails), FindObjectsSortMode.None) as MakerRails[];
+            for (int i = 0; i < RailObjects.Length; i++)
+            {
+                var NewRail = RailObjects[i].gameObject.AddComponent<RailInvis>();
+
+                var Segments = RailObjects[i].GetSegments();
+                List<Rail.Segment> NewSegments = new List<Rail.Segment>();
+
+                for (int j = 0; j < Segments.Count; j++)
+                {
+                    Rail.Segment NewSegment = new Rail.Segment(Vector3.zero, Vector3.up);
+
+                    NewSegment.position = Segments[j].Point;
+                    NewSegment.rotation = Segments[j].Rotation;
+                    NewSegment.distance = Segments[j].Distance;
+
+                    NewSegments.Add(NewSegment);
+                }
+
+                Enum.TryParse(RailObjects[i].Materials.ToString(), out Rail.RailMaterial material1);
+
+                NewRail.material = material1;
+
+                Traverse.Create(NewRail).Field("segments").SetValue(NewSegments);
+            }
+
+            GameObject AIParent = new GameObject("AI Paths Parent");
+            AIParent.transform.parent = levelManager.transform;
+
+            var MakerAIPaths = FindObjectsByType(typeof(MakerAIPath), FindObjectsSortMode.None) as MakerAIPath[];
+            for (int i = 0; i < MakerAIPaths.Length; i++)
+            {
+                //For all path object
+                var NewPath = MakerAIPaths[i].gameObject.AddComponent<AIPath>();
+
+                MakerAIPaths[i].transform.parent = AIParent.transform;
+
+                NewPath.m_points = new List<AIPath.Point>();
+
+                for (int j = 0; j < MakerAIPaths[i].m_points.Count; j++)
+                {
+                    var TempPoint = new AIPath.Point();
+                    TempPoint.position = MakerAIPaths[i].m_points[j].position;
+                    TempPoint.windup = MakerAIPaths[i].m_points[j].windup;
+                    NewPath.m_points.Add(TempPoint);
+                }
+
+                NewPath.CalculateSegments();
+            }
+            Traverse.Create(levelManager).Field("pathsParent").SetValue(AIParent.transform);
+
+
+            GameObject AIPDividerParent = new GameObject("AI Paths Divider Parent");
+            AIPDividerParent.transform.parent = levelManager.transform;
+            var MakerAIPathsDivider = FindObjectsByType(typeof(MakerAIPathDivider), FindObjectsSortMode.None) as MakerAIPathDivider[];
+            for (int i = 0; i < MakerAIPathsDivider.Length; i++)
+            {
+                var NewPath = MakerAIPathsDivider[i].gameObject.AddComponent<AIPathDivider>();
+
+                MakerAIPathsDivider[i].transform.parent = AIParent.transform;
+
+                NewPath.m_points = MakerAIPathsDivider[i].m_points;
+            }
+            Traverse.Create(levelManager).Field("dividersParent").SetValue(AIPDividerParent.transform);
+
+            var TempMakerRaceLine = FindAnyObjectByType<MakerRaceLine>();
+            var TempRaceLine = TempMakerRaceLine.gameObject.AddComponent<RaceLine>();
+
+            TempRaceLine.m_points = new List<RaceLine.Point>();
+
+            for (int i = 0;i < TempMakerRaceLine.m_points.Count;i++)
+            {
+                var RaceLinePoint = new RaceLine.Point();
+
+                RaceLinePoint.position = TempMakerRaceLine.m_points[i];
+
+                TempRaceLine.m_points.Add(RaceLinePoint);
+            }
+
+            TempRaceLine.CalculateSegments();
+
+            Traverse.Create(levelManager).Field("raceLine").SetValue(TempRaceLine);
+        }
+    }
+
+
+    [HarmonyPatch(typeof(MenuManager), "SelectLevel", new System.Type[] { typeof(LevelEntry) })]
+    public class MenuManager_SelectLevel
+    {
+        [HarmonyPrefix]
+        public static void Prefix(ref LevelEntry levelEntry)
+        {
+            if(levelEntry.sceneName.Contains("$"))
+            {
+                levelEntry.sceneName = levelEntry.sceneName.TrimStart('$');
+
+                if (Plugin.LoadedAssetBundle == "")
+                {
+                    Plugin.myLoadedAssetBundle = AssetBundle.LoadFromFile(levelEntry.sceneName);
+                    Plugin.LoadedAssetBundle = levelEntry.sceneName;
+                }
+                else if (Plugin.LoadedAssetBundle != levelEntry.sceneName)
+                {
+                    Plugin.myLoadedAssetBundle.Unload(true);
+                    Plugin.myLoadedAssetBundle = AssetBundle.LoadFromFile(levelEntry.sceneName);
+                    Plugin.LoadedAssetBundle = levelEntry.sceneName;
+                }
+
+                //Load Level
+                string[] scenePath = Plugin.myLoadedAssetBundle.GetAllScenePaths();
+                Plugin.LoadedTrack = scenePath[0];
+
+                var TempSplitName = scenePath[0].Split('\\', '/');
+                string Name = TempSplitName[TempSplitName.Length - 1];
+                levelEntry.sceneName = Name.Split('.')[0];
+            }
+            else
+            {
+                if (Plugin.LoadedAssetBundle != "")
+                {
+                    Plugin.LoadedAssetBundle = "";
+                    Plugin.myLoadedAssetBundle.Unload(true);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TrickyMaddnessLevelHook
+ 
